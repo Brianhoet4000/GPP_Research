cc397078f0a86074f1043e806188432d46e6d657
BrianHoet <113976082+Brianhoet4000@users.noreply.github.com>
2023-08-24 07:02:08 +0200

    Base setup

---
+++ ggp_AiBullet_Research/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MoveSpeed = 10.0f;
+
+    private Rigidbody m_RigidBody;
+
+    private void Awake()
+    {
+        m_RigidBody = gameObject.GetComponent<Rigidbody>();      
+    }
+
+    void FixedUpdate()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+
+       
+        Vector3 movement = movementX * Camera.main.transform.right + movementZ * Camera.main.transform.forward;
+        movement.y = 0.0f;
+        movement.Normalize();
+        movement *= m_MoveSpeed * Time.deltaTime;
+
+        m_RigidBody.velocity = movement;
+    }
+}
