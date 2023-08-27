2d62a6a11a2203360abd86b46e9d0b62d0896cf3
BrianHoet <113976082+Brianhoet4000@users.noreply.github.com>
2023-08-27 17:13:00 +0200

    deleted .idea folder

--- Assets/Scripts/EnemyAccuracyController.cs
+++ Assets/Scripts/EnemyAccuracyController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using LightType = UnityEngine.LightType;
